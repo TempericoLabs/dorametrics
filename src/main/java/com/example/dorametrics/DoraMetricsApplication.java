0f398d9b24b866a2f547acab384082a79a3090bd
TempericoLabs <tempericolabs@gmail.com>
2025-02-22 21:03:24 +0100

    docs: Add DORA metrics description to main application clas11

--- src/main/java/com/example/dorametrics/DoraMetricsApplication.java
+++ src/main/java/com/example/dorametrics/DoraMetricsApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - Time to Restore Service
  * - Change Failure Rate
  *
- * VAMOS
+ * VAMOS 2
  */
 @SpringBootApplication
 public class DoraMetricsApplication {
