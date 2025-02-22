c60cbe710b961c29d06449c04e850de4fe881d98
TempericoLabs <tempericolabs@gmail.com>
2025-02-22 11:25:46 +0100

    docs: Add DORA metrics description to main application class

--- src/main/java/com/example/dorametrics/DoraMetricsApplication.java
+++ src/main/java/com/example/dorametrics/DoraMetricsApplication.java
@@ -3,6 +3,14 @@ package com.example.dorametrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DORA Metrics Application
+ * This application tracks and measures DORA (DevOps Research and Assessment) metrics:
+ * - Deployment Frequency
+ * - Lead Time for Changes
+ * - Time to Restore Service
+ * - Change Failure Rate
+ */
 @SpringBootApplication
 public class DoraMetricsApplication {
     public static void main(String[] args) {
